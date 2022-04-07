dffa1f9d8a31b5ee0a123d1de4518a04ddfecc76
Miles Luders <miles.luders@appfolio.com>
2022-04-07 11:52:02 -0700

    Implement basic UI

---
+++ src/client/components/ActionDetail.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface AppProps {}
+
+const ActionDetail: React.FC<AppProps> = ({}) => {
+  return (
+    <h1>Action Detail</h1>
+  );
+};
+export default ActionDetail;
