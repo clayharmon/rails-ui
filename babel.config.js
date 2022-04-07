5dd1b9540e7278a3f7255e07d5a827e8660ba712
Clay Harmon <clay.harmon@appfolio.com>
2022-04-07 16:07:48 -0400

    Fix babel runtime for async/await

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
+require('regenerator-runtime/runtime');
+require('core-js/stable');
+
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 3,
+        targets: '> 0.25%, not dead'
+      }
+    ],
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-typescript'
   ]
